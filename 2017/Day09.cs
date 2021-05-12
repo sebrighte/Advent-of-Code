c6e9f4840d0d693b3048a337c5f48eeceef4c4cf
Ernest.Sebright <ernest.sebright@ukho.gov.uk>
2021-05-12 15:07:51 +0100

    2017/Day9

--- 2017/Day09.cs
+++ 2017/Day09.cs
@@ -7,12 +7,29 @@ namespace AdventOfCode.Y2017
     [ProblemName("Day9: Stream Processing")]
     class Day09 : BaseLine, Solution
     {
-        public object PartOne(string input) => Day1(input).First();
-        public object PartTwo(string input) => null;// Day1(input).First();
+        public object PartOne(string input) => Solver(input, true).First();
+        public object PartTwo(string input) => Solver(input, false).First();
 
-        private IEnumerable<object> Day1(string inData)
+        private IEnumerable<object> Solver(string inData, bool part1)
         {
-            yield return $"To Do {inData}";
+            int groups = 0;
+            int garbage = 0;
+            int curlyOpen = 0;
+            bool ignoreNext = false;
+            bool startGarbage = false;
+
+            foreach (var charVal in inData)
+            {
+                if (ignoreNext) { ignoreNext = false; continue; }
+                if (startGarbage && charVal != '>' && charVal != '!') { garbage++; continue; }
+                if (charVal == '{') { curlyOpen++; groups += curlyOpen; }
+                if (charVal == '}') if (curlyOpen > 0) curlyOpen--;
+                if (charVal == '<') startGarbage = true;
+                if (charVal == '>') startGarbage = false;
+                if (charVal == '!') ignoreNext = true;
+            }
+
+            yield return part1? groups : garbage;
         }
     }
 }
